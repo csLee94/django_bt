129bb6b35a002b20d1902c03c1f5770e3c1a31a5
ChungSeok-Lee <lcs625@mediance.co.kr>
2022-06-01 23:17:11 +0900

    인바운드 건 생성 기능 추가

--- business_tracking/mbt/urls.py
+++ business_tracking/mbt/urls.py
@@ -7,5 +7,5 @@ app_name = "mbt"
 urlpatterns = [
     path('',views.inbound, name="inbound"),
     path('<int:inbound_id>/', views.inbound_detail, name='inbound_detail'),
-    path('create_inbound/', views.create_inbound, name='create_inbound'),
+    path('inbound/created/', views.create_inbound, name='create_inbound'),
 ]
