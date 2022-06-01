833f6d10d1627af03c5e03a337a49f15256364c6
ChungSeok-Lee <lcs625@mediance.co.kr>
2022-06-01 22:09:26 +0900

    초기 설정 및 임시 테스트

---
+++ business_tracking/mbt/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Inbound
+
+
+def inbound(request):
+    inbound_lst = Inbound.objects.order_by("-created_at")
+    context = {'inbound_list':inbound_lst}
+    return render(request, 'mbt/inbound_list.html', context)
+
+def inbound_detail(request, inbound_id):
+    inbound = Inbound.objects.get(id=inbound_id)
+    context = {'inbound':inbound}
+    return render(request, 'mbt/inbound_detail.html', context)
+
+def create_inbound(request):
+    inbound = Inbound(
+        created_at=timezone.now(),
+        inbound_title=request.POST.get('inbound_title'),
+        client_name=request.POST.get("client_name"),
+        client_contact_point=request.POST.get("client_contact_point"),
+        dept=request.POST.get("dept")
+    )
+    inbound.save()
+    return redirect('mbt:inbound')
